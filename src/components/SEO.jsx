a3bdc730c54b53ad173bcd3a67eb2bba2d631b3a
Ferdous Ahmed <himibaba10@gmail.com>
2026-09-18 04:59:53 +0600

    feat: worked on google search console

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export const SITE_URL = 'https://reactive-ferdous.netlify.app';
+export const SITE_URL = 'https://portfolio.reactiveferdous.com';
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/og-image.png`;
 
 const setMetaByName = (name, content) => {
